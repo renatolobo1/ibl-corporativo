0507dc1e95468da660b312771ddb81742e72b752
Felipe Maciel Ramos Vieira <felipemrvieira@gmail.com>
2019-12-26 15:23:28 -0300

    Cria slideshow com carousel do bootstrap

--- src/pages/Cursos/CursosIndexPage.js
+++ src/pages/Cursos/CursosIndexPage.js
@@ -3,15 +3,17 @@ import './Cursos.scss'
 import Navegacao from '../../components/Navegacao/Navegacao';
 import BarraDeTopo from '../../components/BarraDeTopo/BarraDeTopo';
 import Rodape from '../../components/Rodape/Rodape';
+import SlideShow from '../../components/SlideShow/SlideShow'
 
 class CursosIndexPage extends Component {
-    
+
   render() {
     return (
       <div className="">
         <BarraDeTopo />
         <Navegacao />
-        Conteudo da pagnpm 
+        <SlideShow />
+        Conteudo da pagnpm
         <Rodape />
       </div>
     );
