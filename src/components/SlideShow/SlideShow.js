e4ceb982543d4636a24ebd24b7d45f0afceb0b3c
Felipe Maciel Ramos Vieira <felipemrvieira@gmail.com>
2020-01-03 01:46:32 -0300

    Cria componente form de inscricao

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -8,41 +8,66 @@ class SlideShow extends Component {
 
   render() {
     return (
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={imagem}
-            alt="First slide"
-          />
-          {/* <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption> */}
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={imagem}
-            alt="First slide"
-          />
-          {/* <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption> */}
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={imagem}
-            alt="First slide"
-          />
-          {/* <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption> */}
-        </Carousel.Item>
-      </Carousel>
+      <div id="course-carousel">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={imagem}
+              alt="First slide"
+            />
+            {/* <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption> */}
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={imagem}
+              alt="First slide"
+            />
+            {/* <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption> */}
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={imagem}
+              alt="First slide"
+            />
+            {/* <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption> */}
+          </Carousel.Item>
+        </Carousel>
+        <div className="inscricao">
+          <p>Inscrição Online</p>
+          <input type="text" placeholder="nome"/>
+          <input type="email" placeholder="email"/>
+          <input type="phone" placeholder="Telefone"/>
+          <div className="select-container">
+            <select className="select-inscricao" name="cars">
+              <option value="volvo">Curso</option>
+              <option value="saab">Saab</option>
+              <option value="fiat">Fiat</option>
+              <option value="audi">Audi</option>
+            </select>
+            <select className="select-inscricao" name="cars">
+              <option value="volvo">Faixa Etária</option>
+              <option value="saab">Saab</option>
+              <option value="fiat">Fiat</option>
+              <option value="audi">Audi</option>
+            </select>
+          </div>
+          <div className="button-inscricao">
+            <p>Enviar</p>
+          </div>
+        </div>
+      </div>
     );
   }
 }
