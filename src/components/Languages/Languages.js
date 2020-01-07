70a9b5f1078a99647ccc65de96eacb6304667775
Felipe Maciel <felipemrvieira@gmail.com>
2020-01-07 14:44:42 -0300

    Ajusta responsividade de cursos

--- src/components/Languages/Languages.js
+++ src/components/Languages/Languages.js
@@ -2,115 +2,118 @@ import React, { Component } from 'react';
 import './Languages.scss';
 
 import curso from './curso.jpg'
-import curso1 from './curso1.jpg'
-import curso2 from './curso2.jpg'
-import curso3 from './curso3.jpg'
 import flag from './flag.png'
 
 class Languages extends Component {
 
   render() {
     return (
-      <div id="languages">
-        <aside className="col-3">
-          <div className="conheca">
-            <p>Conheça</p>
-            <p>nossos idiomas</p>
-          </div>
-          <div className="bilingue">
-            <p>Seja</p>
-            <p>Bilingue</p>
-          </div>
-          <div className="botao">
-            <a href="#course-carousel">
-              <p>Quero aprender</p>
-              <p>um novo idioma</p>
-            </a>
-          </div>
-        </aside>
-        <div className="cursos col-9">
-
-          <div className="row">
-
-            {/* Curso */}
-            <a className="curso col-4" href="curso">
-            <div className="curso-info">
-              <div className="text">
-                <p className="title">Inglês</p>
-                <p className="mais">Saiba +</p>
-              </div>
-              <img src={flag} alt="flag"/>
+      <div id="languages" className="container">
+        <div className="row">
+          <aside className="col-md-3">
+            <div className="conheca">
+              <p>Conheça</p>
+              <p>nossos idiomas</p>
             </div>
+            <div className="bilingue">
+              <p>Seja</p>
+              <p>Bilingue</p>
+            </div>
+            <div className="botao">
+              <a href="#course-carousel">
+                <p>Quero aprender</p>
+                <p>um novo idioma</p>
+              </a>
+            </div>
+          </aside>
+          <div className="cursos col-md-9">
 
-            <img className="img-person" src={curso} alt="" srcset=""/>
-          </a>
-          {/* Curso */}
-            {/* Curso */}
-            <a className="curso col-4" href="curso">
-              <div className="curso-info">
-                <div className="text">
-                  <p className="title">Inglês</p>
-                  <p className="mais">Saiba +</p>
+            <div className="container">
+
+
+              <div className="row">
+
+                {/* Curso */}
+                <a className="curso col-md-4" href="curso">
+                <div className="curso-info">
+                  <div className="text">
+                    <p className="title">Inglês</p>
+                    <p className="mais">Saiba +</p>
+                  </div>
+                  <img src={flag} alt="flag"/>
                 </div>
-                <img src={flag} alt="flag"/>
-              </div>
 
-              <img className="img-person" src={curso} alt="" srcset=""/>
-            </a>
-            {/* Curso */}
+                <img className="img-person" src={curso} alt="" srcset=""/>
+              </a>
               {/* Curso */}
-              <a className="curso col-4" href="curso">
-              <div className="curso-info">
-                <div className="text">
-                  <p className="title">Inglês</p>
-                  <p className="mais">Saiba +</p>
-                </div>
-                <img src={flag} alt="flag"/>
+                {/* Curso */}
+                <a className="curso col-md-4" href="curso">
+                  <div className="curso-info">
+                    <div className="text">
+                      <p className="title">Inglês</p>
+                      <p className="mais">Saiba +</p>
+                    </div>
+                    <img src={flag} alt="flag"/>
+                  </div>
+
+                  <img className="img-person" src={curso} alt="" srcset=""/>
+                </a>
+                {/* Curso */}
+                  {/* Curso */}
+                  <a className="curso col-md-4" href="curso">
+                  <div className="curso-info">
+                    <div className="text">
+                      <p className="title">Inglês</p>
+                      <p className="mais">Saiba +</p>
+                    </div>
+                    <img src={flag} alt="flag"/>
+                  </div>
+
+                  <img className="img-person" src={curso} alt="" srcset=""/>
+                </a>
+                {/* Curso */}
+                {/* Curso */}
+                <a className="curso col-md-4" href="curso">
+                  <div className="curso-info">
+                    <div className="text">
+                      <p className="title">Inglês</p>
+                      <p className="mais">Saiba +</p>
+                    </div>
+                    <img src={flag} alt="flag"/>
+                  </div>
+
+                  <img className="img-person" src={curso} alt="" srcset=""/>
+                </a>
+                {/* Curso */}
+                  {/* Curso */}
+                  <a className="curso col-md-4" href="curso">
+                  <div className="curso-info">
+                    <div className="text">
+                      <p className="title">Inglês</p>
+                      <p className="mais">Saiba +</p>
+                    </div>
+                    <img src={flag} alt="flag"/>
+                  </div>
+
+                  <img className="img-person" src={curso} alt="" srcset=""/>
+                </a>
+                {/* Curso */}
+                {/* Curso */}
+                <a className="curso col-md-4" href="curso">
+                  <div className="curso-info">
+                    <div className="text">
+                      <p className="title">Inglês</p>
+                      <p className="mais">Saiba +</p>
+                    </div>
+                    <img src={flag} alt="flag"/>
+                  </div>
+
+                  <img className="img-person" src={curso} alt="" srcset=""/>
+                </a>
+                {/* Curso */}
+
               </div>
-
-              <img className="img-person" src={curso} alt="" srcset=""/>
-            </a>
-            {/* Curso */}
-            {/* Curso */}
-            <a className="curso col-4" href="curso">
-              <div className="curso-info">
-                <div className="text">
-                  <p className="title">Inglês</p>
-                  <p className="mais">Saiba +</p>
-                </div>
-                <img src={flag} alt="flag"/>
-              </div>
-
-              <img className="img-person" src={curso} alt="" srcset=""/>
-            </a>
-            {/* Curso */}
-              {/* Curso */}
-              <a className="curso col-4" href="curso">
-              <div className="curso-info">
-                <div className="text">
-                  <p className="title">Inglês</p>
-                  <p className="mais">Saiba +</p>
-                </div>
-                <img src={flag} alt="flag"/>
-              </div>
-
-              <img className="img-person" src={curso} alt="" srcset=""/>
-            </a>
-            {/* Curso */}
-             {/* Curso */}
-             <a className="curso col-4" href="curso">
-              <div className="curso-info">
-                <div className="text">
-                  <p className="title">Inglês</p>
-                  <p className="mais">Saiba +</p>
-                </div>
-                <img src={flag} alt="flag"/>
-              </div>
-
-              <img className="img-person" src={curso} alt="" srcset=""/>
-            </a>
-            {/* Curso */}
-
+            </div>
           </div>
 
         </div>
