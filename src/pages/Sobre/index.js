6bed26bb3da5d9523533c0c116a68ae244c16fe4
Felipe Maciel <felipemrvieira@gmail.com>
2020-01-22 17:00:44 -0300

    Exibe componentes na pag sobre

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -4,17 +4,15 @@ import './Sobre.scss'
 import Nav from '../../components/Nav/Nav';
 import Topbar from '../../components/Topbar/Topbar';
 import Footer from '../../components/Footer/Footer';
-import Curso from '../../components/Curso/Curso';
 
 class CursosIndexPage extends Component {
 
   render() {
     return (
       <div className="">
-        {/* <Topbar />
+        <Topbar />
         <Nav />
-        <Curso />
-        <Footer /> */}
+        <Footer />
       </div>
     );
   }
