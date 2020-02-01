a98100e20e370cbc216276604360207aa339ab0d
Felipe Maciel Ramos Vieira <felipemrvieira@gmail.com>
2020-02-01 15:01:22 -0300

    Lista unidades

--- src/components/UnitsList/UnitsList.js
+++ src/components/UnitsList/UnitsList.js
@@ -7,9 +7,10 @@ class Unit extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      units: []
+      units: [],
+      states: [],
+      selectedState: 'Alagoas'
     }
-
   }
 
   loadUnits = async () => {
@@ -17,19 +18,36 @@ class Unit extends Component {
       const response = await api.get(`units/index`);
       const units = response.data;
       this.setState({ units });
-      console.log(this.state.units)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  loadStates = async () => {
+    try {
+      const response = await api.get(`units/states`);
+      const states = response.data;
+      this.setState({ states });
     } catch (err) {
       console.log(err);
     }
   }
 
   componentDidMount() {
+    this.loadStates();
     this.loadUnits();
   }
 
+  handleClick = (state) => {
+    this.setState({ selectedState: state })
+  }
+
   renderUnits = () => {
-    const { units } = this.state || [{ id: 1, title: "titulo" }];
-    return units.map(unit => (
+    const selectedUnits = this.state.units.filter(unit => {
+      return unit.address.state === this.state.selectedState;
+    }) ;
+
+    return selectedUnits.map(unit => (
       <li className="unit">
         <Link className="unit-name" to="unidades/gruta">
           <p>{unit.title}</p>
@@ -37,7 +55,7 @@ class Unit extends Component {
         <div className="unit-address-phone">
           <p>{unit.address.street}, {unit.address.number}, {unit.address.neighborhood}</p>
           <p>{unit.address.city} - {unit.address.state}</p>
-          <p>Tel: (82) 3336-8899</p>
+          <p>Tel: {unit.phone}</p>
         </div>
         <div className="unit-site">
           <Link to="unidades/gruta">
@@ -48,13 +66,24 @@ class Unit extends Component {
     ))
   }
 
+  renderStates = () => {
+    const { states } = this.state || [{ id: 1, title: "titulo" }];
+    return states.map(state => (
+      <li>
+        <p onClick={() => this.handleClick(state)}>{state}</p>
+      </li>
+    ))
+  }
+
+
+
   render() {
     return (
       <>
         <div id="units-list" className="container">
           <div className="row">
 
-            <aside className="col-md-3">
+            <aside className="col-md-4">
               <div className="label">
                 <p>Unidades IBL</p>
               </div>
@@ -66,25 +95,10 @@ class Unit extends Component {
             </aside>
             <div className="state-list col-md-3">
               <ul>
-                <li>
-                  <a href="">Alagoas</a>
-                </li>
-                <li>
-                  <a href="">Amapá</a>
-                </li>
-                <li>
-                  <a href="">Ceará</a>
-                </li>
-                <li>
-                  <a href="">Mato Grosso do Sul</a>
-                </li>
-                <li>
-                  <a href="">Paraná</a>
-                </li>
+                {this.renderStates()}
               </ul>
-
             </div>
-            <div className="units-list col-md-6">
+            <div className="units-list col-md-5">
               <ul>
                 {this.renderUnits()}
               </ul>
