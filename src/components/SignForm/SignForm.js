0301de4ea358ebecd32c5722f5d8b892cf1502f2
Felipe Maciel <felipemrvieira@gmail.com>
2020-02-13 12:50:49 -0300

    Renderiza array de idades

--- src/components/SignForm/SignForm.js
+++ src/components/SignForm/SignForm.js
@@ -1,7 +1,46 @@
 import React, { Component } from 'react';
 import './SignForm.scss';
+import api from "../../services/api";
 
 class SignForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          courses: [],
+        }
+    }
+    
+    componentDidMount() {
+        this.loadCourses();
+    }
+    
+    loadCourses = async () => {
+        try {
+          const response = await api.get(`courses/index`);
+          const courses = response.data;
+          this.setState({ courses });
+        } catch (err) {
+          console.log(err);
+        }
+    }
+
+    renderCourses = () => {
+        const { courses } = this.state || [{ id: 1, title: "titulo" }];
+        return courses.map(course => (
+            <option key={course.id} value={course.id}>{course.title}</option>
+        ))
+    }
+
+    renderAges = () => {
+        const ages = [
+            { id: 1, range: "5 - 10 anos" },
+            { id: 2, range: "11 - 20 anos" },
+            { id: 3, range: "21 - 30 anos" },
+        ];
+        return ages.map(age => (
+            <option key={age.id} value={age.id}>{age.range}</option>
+        ))
+    }
 
     render() {
         return (
@@ -12,17 +51,11 @@ class SignForm extends Component {
                 <input type="email" placeholder="email" />
                 <input type="phone" placeholder="Telefone" />
                 <div className="select-container">
-                    <select className="select-inscricao" name="cars">
-                        <option value="volvo">Curso</option>
-                        <option value="saab">Saab</option>
-                        <option value="fiat">Fiat</option>
-                        <option value="audi">Audi</option>
+                    <select className="select-inscricao" name="course">
+                     {this.renderCourses()}
                     </select>
-                    <select className="select-inscricao" name="cars">
-                        <option value="volvo">Faixa Etária</option>
-                        <option value="saab">Saab</option>
-                        <option value="fiat">Fiat</option>
-                        <option value="audi">Audi</option>
+                    <select className="select-inscricao" name="age">
+                        {this.renderAges()}
                     </select>
                 </div>
                 <div className="button-inscricao">
