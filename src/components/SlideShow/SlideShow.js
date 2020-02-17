2ea7a3f668c389ce924221edebb399c372d8f9b4
Felipe Maciel <felipemrvieira@gmail.com>
2020-02-17 16:19:07 -0300

    Renderiza images do slideshow(integracao com api)

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -3,46 +3,57 @@ import './SlideShow.scss';
 import Carousel from 'react-bootstrap/Carousel';
 import imagem from './galeria1.jpg';
 import DiscountForm from '../DiscountForm/DiscountForm';
+import api from "../../services/api";
 
 class SlideShow extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      gallery: {
+        images:[],
+      },
+      redirect: null
+    }
+  }
+
+  componentDidMount() {
+    this.loadGallery();
+  }
+
+  loadGallery = async () => {
+    try {
+      const response = await api.get(`galleries/last`);
+      const gallery = response.data;
+      this.setState({ gallery });
+      console.log(gallery)
+    } catch (err) {
+      console.log(err);
+      this.setState({ redirect: true });
+    }
+  }
+
+  renderGallery = () => {
+    const { images } = this.state.gallery || [{ id: 1, title: "titulo" }];
+    return images.map(image => (
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={image.url}
+          alt="First slide"
+        />
+        {/* <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption> */}
+    </Carousel.Item>
+    ))
+  }
 
   render() {
     return (
       <div id="course-carousel">
         <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={imagem}
-              alt="First slide"
-            />
-            {/* <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption> */}
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={imagem}
-              alt="First slide"
-            />
-            {/* <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption> */}
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={imagem}
-              alt="First slide"
-            />
-            {/* <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption> */}
-          </Carousel.Item>
+          {this.renderGallery()}
         </Carousel>
         <DiscountForm />
       </div>
