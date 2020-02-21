9c68ae75f0ccc4661c243588171db2b603100be7
Felipe Maciel <felipemrvieira@gmail.com>
2020-02-21 15:39:41 -0300

    Finaliza pagina sobre

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 import './About.scss';
 import api from "../../services/api";
 import { HashLink as Link } from 'react-router-hash-link';
-import Modal from 'react-bootstrap/Modal';
+import woman from './11233.png'
+
+import icone1 from './Icone_1.png'
+import icone2 from './Icone_2.png'
+import icone3 from './Icone_3.png'
+import icone4 from './Icone_4.png'
+import icone5 from './Icone_5.png'
+
+import bandeira1 from './Bandeira_01.png'
+import bandeira2 from './Bandeira_02.png'
+import bandeira3 from './Bandeira_03.png'
+import bandeira4 from './Bandeira_04.png'
+import bandeira5 from './Bandeira_05.png'
+import bandeira6 from './Bandeira_06.png'
 
 
 class About extends Component {
@@ -10,72 +23,31 @@ class About extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      activeModal: null,
-      courses: [],
+      page: {},
     }
-    this.clickHandler = this.clickHandler.bind(this);
-    this.hideModal = this.hideModal.bind(this);
   }
 
   componentDidMount() {
-    this.loadCourses();
+    this.loadPage();
   }
 
-  loadCourses = async () => {
+  loadPage = async () => {
     try {
-      const response = await api.get(`courses/index`);
-      const courses = response.data;
-      this.setState({ courses });
+      const response = await api.get(`pages/sobre`);
+      const page = response.data;
+      this.setState({ page });
       // console.log(this.state.courses)
     } catch (err) {
       console.log(err);
     }
   }
 
-  renderStates = () => {
-    const { states } = this.state || [{ id: 1, title: "titulo" }];
-    return states.map(state => (
-      <li>
-        <p onClick={() => this.handleClick(state)}>{state}</p>
-      </li>
-    ))
-  }
-
-  clickHandler(e, index) {
-    this.setState({ ...this.state, activeModal: index })
-  }
-
-  hideModal(e) {
-    this.setState({ ...this.state, activeModal: null })
-  }
-
-  renderCourse() {
-    const coursesList = this.state.courses
-
-    return coursesList.map((course, index) => (
-      <div key={index} className="curso col-md-4" onClick={e => this.clickHandler(e, index)}>
-        <div className="curso-info">
-          <div className="text">
-            <p className="title">{course.title}</p>
-            <p className="mais">Saiba +</p>
-          </div>
-        </div>
-
-        <img className="img-person" src={course.cover.url} alt="" srcSet="" />
-        <div onClick={e => e.stopPropagation()}>
-
-        </div>
-
-
-      </div>
-    ))
-  }
-
   render() {
     return (
       <div id="about" >
-        <div className="container">
-          <div className="row">
+        <div className="container" style={{backgroundImage: `url(${woman})`}}>
+          <div className="row" >
+            
             <aside className="col-md-3">
               <div className="sobre">
                 <p>Sobre</p>
@@ -85,20 +57,37 @@ class About extends Component {
                 <p>História</p>
               </div>
             </aside>
-            <div className="cursos col-md-6">
-
+            
+            <div className="col-md-6">
               <div className="container">
                 <div className="row">
-                  {this.renderCourse()}
+                  <div className=""
+                    dangerouslySetInnerHTML={{ __html: this.state.page.body }} />
+                    <h3>Nossos Cursos</h3>
+                   <div className="cursos">
+                    <img src={bandeira1} />
+                    <img src={bandeira2} />
+                    <img src={bandeira3} />
+                    <img src={bandeira4} />
+                    <img src={bandeira5} />
+                    <img src={bandeira6} />
+                  </div>
                 </div>
               </div>
             </div>
 
-            <div className="col-md-6">
-
+            <div className="col-md-3">
             </div>
 
           </div>
+
+        </div>
+        <div className="detalhes">
+          <img src={icone1} />
+          <img src={icone2} />
+          <img src={icone3} />
+          <img src={icone4} />
+          <img src={icone5} />
         </div>
       </div>
     );
