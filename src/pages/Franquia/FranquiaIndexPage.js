58e69dd7cc93c4390420a360549024b26801cad3
Felipe Maciel Ramos Vieira <felipemrvieira@gmail.com>
2020-04-29 09:38:19 -0300

    Remove requisição inicial ao servidor(feito pelo bug do cors)

--- src/pages/Franquia/FranquiaIndexPage.js
+++ src/pages/Franquia/FranquiaIndexPage.js
@@ -12,22 +12,8 @@ import Languages from '../../components/Languages/Languages';
 import Topbar from '../../components/Topbar/Topbar';
 import Footer from '../../components/Footer/Footer';
 import FindUnit from '../../components/findUnit/findUnit';
-import axios from "axios";
 
 class FranquiaIndexPage extends Component {
-
-  componentDidMount() {
-    this.load();
-  }
-
-  load = async () => {
-    try {
-      await axios.get(`https://www.iblsemlegenda.com.br`);
-      // alert(response.status)
-    } catch (err) {
-      console.log(err);
-    }
-  }
 
   render() {
     return (
