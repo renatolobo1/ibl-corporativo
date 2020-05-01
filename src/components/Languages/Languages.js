436e71317b86de1938b3751254b92982796c3f07
Felipe Maciel Ramos Vieira <felipemrvieira@gmail.com>
2020-05-01 15:19:15 -0300

    Corrige escrita

--- src/components/Languages/Languages.js
+++ src/components/Languages/Languages.js
@@ -127,7 +127,7 @@ class Languages extends Component {
                             <div className="col-md-6">
                               <div id="inscricao" className="botao-curso">
                                 <Link to="#topbar" onClick={this.hideModal}>
-                                  <p className="label">Faça sua inscriçao e</p>
+                                  <p className="label">Faça sua inscrição e</p>
                                   <p className="info">estude {course.title}</p>
                                 </Link>
                               </div>
